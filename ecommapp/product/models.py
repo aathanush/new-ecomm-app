3929844c39b0de2243344007fbea3dab9989e515
Thanush A A <aathanush@gmail.com>
2023-03-07 15:12:44 +0530

    Did some migrations

--- ecommapp/product/models.py
+++ ecommapp/product/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.DecimalField(max_digits=100000, decimal_places=2)
+    quantity = models.DecimalField(decimal_places=0,max_digits=10000) 
